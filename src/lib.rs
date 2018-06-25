dc3c0b2002b7da8efebf261579208d48ff100c42
Colin Barré-Brisebois <colinbb@gmail.com>
2018-06-25 17:40:00 +0200

    :art: Fix tabs vs spaces

--- src/lib.rs
+++ src/lib.rs
@@ -37,203 +37,203 @@ pub use triangle::*;
 pub use vector::*;
 
 pub trait Traceable: Send + Sync {
-    fn intersect(&self, ray: &Ray, result: &mut Hit) -> bool;
+	fn intersect(&self, ray: &Ray, result: &mut Hit) -> bool;
 }
 
 pub fn trace(
-    scene: &Scene,
-    camera: &Camera,
-    width: usize,
-    height: usize,
-    samples: u32,
-    backbuffer: &mut [Float3],
-    rays: &mut usize,
+	scene: &Scene,
+	camera: &Camera,
+	width: usize,
+	height: usize,
+	samples: u32,
+	backbuffer: &mut [Float3],
+	rays: &mut usize,
 ) {
-    let ray_count = AtomicUsize::new(0);
-    let inv_width = 1.0 / width as f32;
-    let inv_height = 1.0 / height as f32;
-    let inv_samples = 1.0 / samples as f32;
+	let ray_count = AtomicUsize::new(0);
+	let inv_width = 1.0 / width as f32;
+	let inv_height = 1.0 / height as f32;
+	let inv_samples = 1.0 / samples as f32;
 
-    // For each row of pixels
-    backbuffer
-        .par_chunks_mut(width as usize)
-        .enumerate()
-        .for_each(|(j, row)| {
-            row.iter_mut().enumerate().for_each(|(i, output)| {
-                let mut radiance = Float3::zero();
-                let mut num_rays = 0;
-                let mut rng = thread_rng();
+	// For each row of pixels
+	backbuffer
+		.par_chunks_mut(width as usize)
+		.enumerate()
+		.for_each(|(j, row)| {
+			row.iter_mut().enumerate().for_each(|(i, output)| {
+				let mut radiance = Float3::zero();
+				let mut num_rays = 0;
+				let mut rng = thread_rng();
 
-                for _ in 0..samples {
-                    let rnd_x: f32 = rng.gen();
-                    let rnd_y: f32 = rng.gen();
-                    let dx = ((i as f32 + rnd_x) * inv_width) - 0.5;
-                    let dy = ((j as f32 + rnd_y) * inv_height) - 0.5;
+				for _ in 0..samples {
+					let rnd_x: f32 = rng.gen();
+					let rnd_y: f32 = rng.gen();
+					let dx = ((i as f32 + rnd_x) * inv_width) - 0.5;
+					let dy = ((j as f32 + rnd_y) * inv_height) - 0.5;
 
-                    // Compute V
-                    let v = camera.forward + camera.right * dx - camera.up * dy;
+					// Compute V
+					let v = camera.forward + camera.right * dx - camera.up * dy;
 
-                    // Spawn a ray
-                    let ray = Ray {
-                        origin: camera.origin + v * 10.0,
-                        direction: v.normalize(),
-                    };
+					// Spawn a ray
+					let ray = Ray {
+						origin: camera.origin + v * 10.0,
+						direction: v.normalize(),
+					};
 
-                    radiance += compute_radiance(ray, &scene, 0, &mut num_rays);
-                }
+					radiance += compute_radiance(ray, &scene, 0, &mut num_rays);
+				}
 
-                ray_count.fetch_add(num_rays, Ordering::Relaxed);
-                *output = radiance * inv_samples;
-            });
-        });
+				ray_count.fetch_add(num_rays, Ordering::Relaxed);
+				*output = radiance * inv_samples;
+			});
+		});
 
-    *rays = ray_count.load(Ordering::Relaxed);
+	*rays = ray_count.load(Ordering::Relaxed);
 }
 
 fn luminance(color: Float3) -> f32 {
-    0.299 * color.get_x() + 0.587 * color.get_y() + 0.114 * color.get_z()
+	0.299 * color.get_x() + 0.587 * color.get_y() + 0.114 * color.get_z()
 }
 
 fn compute_radiance(ray: Ray, scene: &Scene, depth: i32, num_rays: &mut usize) -> Float3 {
-    *num_rays += 1;
-    let intersect: Option<Hit> = scene.intersect(ray);
+	*num_rays += 1;
+	let intersect: Option<Hit> = scene.intersect(ray);
 
-    match intersect {
-        None => Float3::zero(),
-        Some(hit) => {
-            let position = ray.origin + ray.direction * hit.t;
-            let normal = hit.n;
+	match intersect {
+		None => Float3::zero(),
+		Some(hit) => {
+			let position = ray.origin + ray.direction * hit.t;
+			let normal = hit.n;
 
-            let mut f = hit.material.albedo;
-            if depth > 3 {
-                if rand::random::<f32>() < luminance(f) && depth < 10 {
-                    f = f / luminance(f);
-                } else {
-                    return hit.material.emission;
-                }
-            }
+			let mut f = hit.material.albedo;
+			if depth > 3 {
+				if rand::random::<f32>() < luminance(f) && depth < 10 {
+					f = f / luminance(f);
+				} else {
+					return hit.material.emission;
+				}
+			}
 
-            let irradiance: Float3 = match hit.material.bsdf {
-                // Diffuse Reflection
-                BSDF::Diffuse => {
-                    // Sample cosine distribution and transform into world tangent space
-                    let r1 = 2.0 * PI * rand::random::<f32>();
-                    let r2 = rand::random::<f32>();
-                    let r2s = r2.sqrt();
-                    let w_up = if normal.get_x().abs() > 0.1 {
-                        Float3::new(0.0, 1.0, 0.0)
-                    } else {
-                        Float3::new(1.0, 0.0, 0.0)
-                    };
+			let irradiance: Float3 = match hit.material.bsdf {
+				// Diffuse Reflection
+				BSDF::Diffuse => {
+					// Sample cosine distribution and transform into world tangent space
+					let r1 = 2.0 * PI * rand::random::<f32>();
+					let r2 = rand::random::<f32>();
+					let r2s = r2.sqrt();
+					let w_up = if normal.get_x().abs() > 0.1 {
+						Float3::new(0.0, 1.0, 0.0)
+					} else {
+						Float3::new(1.0, 0.0, 0.0)
+					};
 
-                    let tangent = normal.cross(w_up).normalize();
-                    let bitangent = normal.cross(tangent).normalize();
-                    let next_direction = tangent * r1.cos() * r2s
-                        + bitangent * r1.sin() * r2s
-                        + normal * (1.0 - r2).sqrt();
+					let tangent = normal.cross(w_up).normalize();
+					let bitangent = normal.cross(tangent).normalize();
+					let next_direction = tangent * r1.cos() * r2s
+						+ bitangent * r1.sin() * r2s
+						+ normal * (1.0 - r2).sqrt();
 
-                    compute_radiance(
-                        Ray::new(position, next_direction.normalize()),
-                        scene,
-                        depth + 1,
-                        num_rays,
-                    )
-                }
+					compute_radiance(
+						Ray::new(position, next_direction.normalize()),
+						scene,
+						depth + 1,
+						num_rays,
+					)
+				}
 
-                // Mirror Reflection
-                BSDF::Mirror => {
-                    let r = ray.direction.normalize()
-                        - normal.normalize() * 2.0 * normal.dot(ray.direction);
+				// Mirror Reflection
+				BSDF::Mirror => {
+					let r = ray.direction.normalize()
+						- normal.normalize() * 2.0 * normal.dot(ray.direction);
 
-                    compute_radiance(
-                        Ray::new(position, r.normalize()),
-                        scene,
-                        depth + 1,
-                        num_rays,
-                    )
-                }
+					compute_radiance(
+						Ray::new(position, r.normalize()),
+						scene,
+						depth + 1,
+						num_rays,
+					)
+				}
 
-                // Glass / Translucent
-                BSDF::Glass => {
-                    let r = ray.direction.normalize()
-                        - normal.normalize() * 2.0 * normal.dot(ray.direction);
-                    let reflection = Ray::new(position, r);
+				// Glass / Translucent
+				BSDF::Glass => {
+					let r = ray.direction.normalize()
+						- normal.normalize() * 2.0 * normal.dot(ray.direction);
+					let reflection = Ray::new(position, r);
 
-                    // Compute input-output IOR
-                    let into = normal.dot(normal) > 0.0;
-                    let nc = 1.0;
-                    let nt = 1.5;
-                    let nnt = if into { nc / nt } else { nt / nc };
+					// Compute input-output IOR
+					let into = normal.dot(normal) > 0.0;
+					let nc = 1.0;
+					let nt = 1.5;
+					let nnt = if into { nc / nt } else { nt / nc };
 
-                    // Compute fresnel
-                    let ddn = ray.direction.dot(normal);
-                    let cos2t = 1.0 - nnt * nnt * (1.0 - ddn * ddn);
+					// Compute fresnel
+					let ddn = ray.direction.dot(normal);
+					let cos2t = 1.0 - nnt * nnt * (1.0 - ddn * ddn);
 
-                    if cos2t < 0.0 {
-                        // Total internal reflection
-                        compute_radiance(reflection, scene, depth + 1, num_rays)
-                    } else {
-                        let transmitted_dir = (ray.direction * nnt
-                            - normal
-                                * (if into { 1.0 } else { -1.0 } * (ddn * nnt + cos2t.sqrt())))
-                            .normalize();
-                        let transmitted_ray = Ray::new(position, transmitted_dir);
+					if cos2t < 0.0 {
+						// Total internal reflection
+						compute_radiance(reflection, scene, depth + 1, num_rays)
+					} else {
+						let transmitted_dir = (ray.direction * nnt
+							- normal
+								* (if into { 1.0 } else { -1.0 } * (ddn * nnt + cos2t.sqrt())))
+							.normalize();
+						let transmitted_ray = Ray::new(position, transmitted_dir);
 
-                        let a = nt - nc;
-                        let b = nt + nc;
-                        let base_reflectance = a * a / (b * b);
-                        let c = 1.0 - if into {
-                            -ddn
-                        } else {
-                            transmitted_dir.dot(normal)
-                        };
+						let a = nt - nc;
+						let b = nt + nc;
+						let base_reflectance = a * a / (b * b);
+						let c = 1.0 - if into {
+							-ddn
+						} else {
+							transmitted_dir.dot(normal)
+						};
 
-                        let reflectance =
-                            base_reflectance + (1.0 - base_reflectance) * c * c * c * c * c;
-                        let transmittance = 1.0 - reflectance;
-                        let rr_propability = 0.25 + 0.5 * reflectance;
-                        let reflectance_propability = reflectance / rr_propability;
-                        let transmittance_propability = transmittance / (1.0 - rr_propability);
+						let reflectance =
+							base_reflectance + (1.0 - base_reflectance) * c * c * c * c * c;
+						let transmittance = 1.0 - reflectance;
+						let rr_propability = 0.25 + 0.5 * reflectance;
+						let reflectance_propability = reflectance / rr_propability;
+						let transmittance_propability = transmittance / (1.0 - rr_propability);
 
-                        if depth > 1 {
-                            // Russian roulette between reflectance and transmittance
-                            if rand::random::<f32>() < rr_propability {
-                                compute_radiance(reflection, scene, depth + 1, num_rays)
-                                    * reflectance_propability
-                            } else {
-                                compute_radiance(transmitted_ray, scene, depth + 1, num_rays)
-                                    * transmittance_propability
-                            }
-                        } else {
-                            compute_radiance(reflection, scene, depth + 1, num_rays) * reflectance
-                                + compute_radiance(transmitted_ray, scene, depth + 1, num_rays)
-                                    * transmittance
-                        }
-                    }
-                }
-            };
+						if depth > 1 {
+							// Russian roulette between reflectance and transmittance
+							if rand::random::<f32>() < rr_propability {
+								compute_radiance(reflection, scene, depth + 1, num_rays)
+									* reflectance_propability
+							} else {
+								compute_radiance(transmitted_ray, scene, depth + 1, num_rays)
+									* transmittance_propability
+							}
+						} else {
+							compute_radiance(reflection, scene, depth + 1, num_rays) * reflectance
+								+ compute_radiance(transmitted_ray, scene, depth + 1, num_rays)
+									* transmittance
+						}
+					}
+				}
+			};
 
-            return irradiance * f + hit.material.emission;
-        }
-    }
+			return irradiance * f + hit.material.emission;
+		}
+	}
 }
 
 // todo: remove me later
 
 pub fn saturate(color: Float3) -> Float3 {
-    Float3::new(
-        color.get_x().max(0.0).min(1.0),
-        color.get_y().max(0.0).min(1.0),
-        color.get_z().max(0.0).min(1.0),
-    )
+	Float3::new(
+		color.get_x().max(0.0).min(1.0),
+		color.get_y().max(0.0).min(1.0),
+		color.get_z().max(0.0).min(1.0),
+	)
 }
 
 pub fn tonemap(color: Float3) -> Float3 {
-    let color_linear = Float3::new(
-        color.get_x().powf(1.0 / 2.2),
-        color.get_y().powf(1.0 / 2.2),
-        color.get_z().powf(1.0 / 2.2),
-    );
+	let color_linear = Float3::new(
+		color.get_x().powf(1.0 / 2.2),
+		color.get_y().powf(1.0 / 2.2),
+		color.get_z().powf(1.0 / 2.2),
+	);
 
-    return saturate(color_linear);
+	return saturate(color_linear);
 }
--- src/plane.rs
+++ src/plane.rs
@@ -6,41 +6,41 @@ use Traceable;
 
 #[derive(Copy, Clone)]
 pub struct Plane {
-    pub position: Float3,
-    pub normal: Float3,
-    pub material: Material,
+	pub position: Float3,
+	pub normal: Float3,
+	pub material: Material,
 }
 
 impl Plane {
-    // Spawn a new plane
-    pub fn new(position: Float3, normal: Float3, material: Material) -> Plane {
-        Plane {
-            position: position,
-            normal: normal,
-            material: material,
-        }
-    }
+	// Spawn a new plane
+	pub fn new(position: Float3, normal: Float3, material: Material) -> Plane {
+		Plane {
+			position: position,
+			normal: normal,
+			material: material,
+		}
+	}
 }
 
 impl Traceable for Plane {
-    // Ray-Plane Intersection
-    fn intersect(&self, r: &Ray, result: &mut ::Hit) -> bool {
-        let plane_normal = -self.normal;
-        let denom = plane_normal.dot(r.direction);
+	// Ray-Plane Intersection
+	fn intersect(&self, r: &Ray, result: &mut ::Hit) -> bool {
+		let plane_normal = -self.normal;
+		let denom = plane_normal.dot(r.direction);
 
-        if denom > 1e-6 {
-            result.t = plane_normal.dot(self.position - r.origin) / denom;
-            result.p = r.origin + r.direction * result.t;
-            result.n = if Float3::dot(self.normal, r.direction) < 0.0 {
-                self.normal
-            } else {
-                -self.normal
-            };
-            result.material = self.material;
+		if denom > 1e-6 {
+			result.t = plane_normal.dot(self.position - r.origin) / denom;
+			result.p = r.origin + r.direction * result.t;
+			result.n = if Float3::dot(self.normal, r.direction) < 0.0 {
+				self.normal
+			} else {
+				-self.normal
+			};
+			result.material = self.material;
 
-            return true;
-        } else {
-            return false;
-        }
-    }
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
